030955729a9c8e4bd8dde3a7bf92c02b2226a18d
lelion13 <lelion13@gmail.com>
2024-07-22 15:18:52 -0300

    carrusel sin js

--- js/correo.js
+++ js/correo.js
@@ -6,3 +6,25 @@
 //     message: "hola",
 //     reply_to: "lelion_13@hotmail.com",
 //     });
+
+const btn = document.getElementById('enviar');
+
+document.getElementById('formulario')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'service_o5e397c';
+   const templateID = 'template_gz3479r';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('Correo enviado, nos contactaremos a la brevedad!');
+      document.getElementById('formulario').reset();
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
