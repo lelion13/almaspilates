62b167118ced6e5bfd0ad96af075ffa1a418f78d
lelion13 <lelion13@gmail.com>
2024-07-22 00:00:07 -0300

    modificacion de contacto

---
+++ js/correo.js
@@ -0,0 +1,8 @@
+
+// Esto lo copie desde el template de emailjs
+// emailjs.send("service_o5e397c","template_gz3479r",{
+//     to_name: "lelion13@gmail.com",
+//     from_name: "llion@cpmgsa.ar",
+//     message: "hola",
+//     reply_to: "lelion_13@hotmail.com",
+//     });
