0dc30a2f8aa7e31df52fc7fe75b615c18c0c9de3
lelion13 <lelion13@gmail.com>
2024-07-29 21:58:32 -0300

    cambio en el control del formulario de contacto y headers

--- js/contacto.js
+++ js/contacto.js
@@ -38,3 +38,60 @@ document.getElementById('email').addEventListener('blur', function() {
       emailField.style.marginBottom = '16px';
   }
 });
+
+//valido formato del nombre
+
+document.getElementById('name').addEventListener('blur', function() {
+  var nameField = document.getElementById('name');
+  var nameError = document.getElementById('nameError');
+  var namePattern = /^[A-Za-z]$/i;
+
+  if (!namePattern.test(nameField.value)) {
+      nameError.style.display = 'inline';
+      nameField.style.borderColor = 'red';
+      nameField.style.marginBottom = 0;
+  } else {
+      nameError.style.display = 'none';
+      nameField.style.borderColor = 'initial';
+      nameField.style.marginBottom = '16px';
+  }
+});
+
+//valido formato de apellido
+
+document.getElementById('surname').addEventListener('blur', function() {
+  var surnameField = document.getElementById('surname');
+  var surnameError = document.getElementById('surnameError');
+  var surnamePattern = /^[A-Za-z]$/i;
+
+  if (!surnamePattern.test(surnameField.value)) {
+      surnameError.style.display = 'inline';
+      surnameField.style.borderColor = 'red';
+      surnameField.style.marginBottom = 0;
+  } else {
+      surnameError.style.display = 'none';
+      surnameField.style.borderColor = 'initial';
+      surnameField.style.marginBottom = '16px';
+  }
+});
+
+//Limpio los label de error!
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("formulario");
+  const resetButton = document.getElementById("cancelar");
+
+  resetButton.addEventListener("click", function(event) {
+      // Ocultar mensajes de error
+
+      const inputs = form.querySelectorAll("input, textarea, select");
+                inputs.forEach(input => {
+                    input.style.borderColor = '#ccc';
+                    input.style.marginBottom = '16px'; // Restablecer el margen inferior si se cambió
+                });
+      document.getElementById("nameError").style.display = "none";
+      document.getElementById("surnameError").style.display = "none";
+      document.getElementById("emailError").style.display = "none";
+      
+      // Si deseas realizar otras acciones, puedes agregarlas aquí
+  });
+});
